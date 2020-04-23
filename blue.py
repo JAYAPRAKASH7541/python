23df713941d273e53d807cf9d205edaf745cd62f
JAYAPRAKASH7541 <42533294+JAYAPRAKASH7541@users.noreply.github.com>
2020-04-23 17:31:27 +0530

    Create blue.py

---
+++ blue.py
@@ -0,0 +1,114 @@
+Software Requirements:
+-----------------------
+1)Reading Question(end-end)
+2)List all the classes
+3)Specify attributes of classes
+4)List all the Actions/Methods
+5)Adding tags to attributes(private,public,protected)
+6)Assign tags to methods(instance,class,static)
+7)encapsulation
+8)Inheritance
+
+Classes
+-------
+1)Deer
+2)Lion
+3)Shark
+4)GoldFish
+5)Zoo
+6)Snake
+
+Attributes  and Methods of each class
+-----------------------
+Deer class:
+-----------
+Attributes:
+-----------
+1)Age in months//Tag-Protected
+2)Breed
+3)Required food in kgs
+
+Methods:
+---------
+1)Breathe
+2)Make Sound
+3)Grow
+
+Lion class
+---------
+Attributes
+-----------
+1)Age in months
+2)Breed
+3)Required food in kgs
+
+Methods:
+---------
+1)Breathe
+2)Make Sound
+3)Grow
+4)Hunt
+
+shark class
+-----------
+Attributes
+-----------
+1)Age in months
+2)Breed
+3)Required food in kgs
+
+Methods:
+---------
+1)Breathe
+2)Make Sound
+3)Grow
+4)Hunt
+
+
+GoldFish
+--------
+Attributes
+-----------
+1)Age in months
+2)Breed
+3)Required food in kgs
+
+Methods:
+---------
+1)Breathe
+2)Make Sound
+3)Grow
+
+Zoo
+-----
+Attributes
+-----------
+1)Reserved food in kgs
+
+Methods
+--------
+1)Count_animals
+2)Add_food_to_reserve
+3)Add_animal
+4)count_animals_in_zoos
+5)count_animals_in_given_Zoo
+6)feed
+
+Snake
+------
+Attributes
+-----------
+1)Age in months
+2)Breed
+3)Required food in kgs
+
+Methods:
+---------
+1)Breathe
+2)Make Sound
+3)Grow
+4)Hunt
+
+
+
+
